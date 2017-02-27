27965c72d043891e3c30a8b02f706f0988731227
Tony <clc18@gmail.com>
2017-02-27 15:26:50 +0000

    implementing basic structure

--- main.cpp
+++ main.cpp
@@ -12,14 +12,57 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
 
+int graph[100][100], heuristic[100][100][3], iterations; 
+/*
+ * - graph matrix display represent each nodes of the graph
+ * - heuristic matrix represent the vertices of the graph with heuristics(1 distance and 2 pheromone level)
+ *   E.g.: graph[54][21][1] - has the distance between nodes 54 and 21;
+ *         graph[54][21][2] - has the pheromone level of the vertex between 
+ * - iterations represent the number of cycle the program will perform 
+ */
+
+void initialize_matrices()
+{
+    
+}
+
+void group_nodes()
+{
+    
+}
+
+void locate_ants()
+{
+    
+}
+
+void next_city(int &x, int&y)
+{
+    
+}
+
+int start_aco()
+{
+    
+    for(int i=1 ; i<=iterations; i++)
+    {
+        locate_ants();
+        next_city();
+    }
+    return 0; 
+}
+   
+
+int main(int argc, char** argv) {
     return 0;
 }
 
