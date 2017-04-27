603fb359ab8057e9d57a0a19ed4a03f203df2b7d
clc18 <clc18@psvws031.pau.local>
2017-04-27 11:35:07 +0100

    refactoring

---
+++ cluster.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   cluster.h
+ * Author: clc18
+ *
+ * Created on 27 April 2017, 10:52
+ */
+
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+
+
+#endif /* CLUSTER_H */
+
